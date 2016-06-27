5e18e2b5d30dd3b2fa3b892afb7bc5fe8b3acda3
iwase22334 <iwase22334@gmail.com>
2016-06-28 00:16:43 +0900

    add seed_user command

--- src/ROSIKClient.cpp
+++ src/ROSIKClient.cpp
@@ -2,19 +2,25 @@
 // Include
 //----------------------------------------------------------
 #include "ROSIKClient.hpp"
-
+//----------------------------------------------------------
+// Constant parameter
+//----------------------------------------------------------
 const int ROSIKClient::SEED_AUTO(0);
 const int ROSIKClient::SEED_USER(1);
 const int ROSIKClient::SEED_CURRENT(2);
 const int ROSIKClient::SEED_NS_MAP(3);
+
+// Names in arm joints
+const std::array<std::string, ROSIKClient::JOINTS_NUM> ROSIKClient::LEFT_JOINT_NAMES = {"left_s0", "left_s1", "left_e0", "left_e1", "left_w0", "left_w1", "left_w2"};
+const std::array<std::string, ROSIKClient::JOINTS_NUM> ROSIKClient::RIGHT_JOINT_NAMES = {"right_s0", "right_s1", "right_e0", "right_e1", "right_w0", "right_w1", "right_w2"};
+
 //----------------------------------------------------------
-// Constant parameter
+// LRRequest
 //----------------------------------------------------------
 ROSIKClient::LRRequest::LRRequest(const BCMSSolvePositionIK& r) : BCMSSolvePositionIK(r) {};
 ROSIKClient::LRRequest::LRRequest(const int mode, const std::array<double, 3>& x, const std::array<double, 4>& q)
 {
-	assert(x.size() == 3);
-	assert(q.size() == 4);
+	assert(mode != SEED_USER);
 
 	request.pose_stamp.resize(1);
 	request.pose_stamp[0].header.stamp = ros::Time::now();
@@ -29,12 +35,51 @@ ROSIKClient::LRRequest::LRRequest(const int mode, const std::array<double, 3>& x
 
 	request.seed_mode = mode;
 };
+ROSIKClient::LRRequest::LRRequest(const int mode, const std::array<double, 7>& seed_angles, const std::array<double, 3>& x, const std::array<double, 4>& q)
+{
+	assert(mode == SEED_USER);
 
+	request.pose_stamp.resize(1);
+	request.pose_stamp[0].header.stamp = ros::Time::now();
+	request.pose_stamp[0].header.frame_id = "base";
+	request.pose_stamp[0].pose.position.x = x[0];
+	request.pose_stamp[0].pose.position.y = x[1];
+	request.pose_stamp[0].pose.position.z = x[2];
+	request.pose_stamp[0].pose.orientation.x = q[0];
+	request.pose_stamp[0].pose.orientation.y = q[1];
+	request.pose_stamp[0].pose.orientation.z = q[2];
+	request.pose_stamp[0].pose.orientation.w = q[3];
+
+	request.seed_angles.resize(1);
+	request.seed_angles[0].position.resize(JOINTS_NUM);
+	copy_n(seed_angles.begin(), JOINTS_NUM, request.seed_angles[0].position.begin());
+
+	request.seed_mode = mode;
+
+};
+//----------------------------------------------------------
+// LeftRequest
+//----------------------------------------------------------
 ROSIKClient::LeftRequest::LeftRequest(const BCMSSolvePositionIK& r) : ROSIKClient::LRRequest(r){};
 ROSIKClient::LeftRequest::LeftRequest(const int mode, const std::array<double, 3>& x, const std::array<double, 4>& q) : ROSIKClient::LRRequest(mode, x, q){};
+ROSIKClient::LeftRequest::LeftRequest(const int mode, const std::array<double, 7>& seed_angles, const std::array<double, 3>& x, const std::array<double, 4>& q) 
+	: ROSIKClient::LRRequest(mode, seed_angles, x, q)
+{
+	request.seed_angles[0].name.resize(JOINTS_NUM);
+	copy_n(LEFT_JOINT_NAMES.begin(), JOINTS_NUM, request.seed_angles[0].name.begin());
+};
 
+//----------------------------------------------------------
+// RightRequest
+//----------------------------------------------------------
 ROSIKClient::RightRequest::RightRequest(const BCMSSolvePositionIK& r) : ROSIKClient::LRRequest(r){};
 ROSIKClient::RightRequest::RightRequest(const int mode, const std::array<double, 3>& x, const std::array<double, 4>& q) : ROSIKClient::LRRequest(mode, x, q){};
+ROSIKClient::RightRequest::RightRequest(const int mode, const std::array<double, 7>& seed_angles, const std::array<double, 3>& x, const std::array<double, 4>& q) 
+	: ROSIKClient::LRRequest(mode, seed_angles, x, q)
+{	
+	request.seed_angles[0].name.resize(JOINTS_NUM);
+	copy_n(RIGHT_JOINT_NAMES.begin(), JOINTS_NUM, request.seed_angles[0].name.begin());
+};
 
 //----------------------------------------------------------
 // ROSIKClient
