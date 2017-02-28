6372aaa76c977e47a7b552631f65a74e416e3de3
iwase22334 <iwase22334@gmail.com>
2017-02-28 16:06:09 +0900

    add comment

--- include/ros_module/ROSInterface.hpp
+++ include/ros_module/ROSInterface.hpp
@@ -3,6 +3,39 @@
  * @brief Interface Library for easy access to the ROS
  * @author Iwase
  * @date 2016.9.26.
+ * @mainpage 
+ * - This project consists of roughly 7 modules.
+ * -# ROSFramework.hpp
+ * -# ROSHookController.hpp
+ * -# ROSJointController.hpp
+ * -# ROSIKClient.hpp
+ * -# ROSTFListener.hpp
+ * -# ROSTFBroadcaster.hpp
+ * -# Others
+ * 
+ * @section main_sec1 Using Basic functions of ros.
+ * - \ref ROSSubscriberInterface< T >
+ *  - sample program : sample_subscribe.cpp
+ * - \ref ROSServiceInterface< T >
+ * @section main_sec2 Using gripper.
+ * - \ref ROSHookController
+ *  - sample program : sample_hook.cpp
+ * @section main_sec3 Using baxter arm.
+ * - \ref ROSJointController
+ *  - sample program : sample_arm.cpp
+ * @section main_sec4 Using ik solver.
+ * - \ref ROSIKClient
+ *  - sample program : sample_ik.cpp
+ * @section main_sec5 using tf.
+ * - \ref ROSTFListener
+ *  - sample program : sample_tf_listen.cpp
+ * - \ref ROSTFBroadcaster
+ *  - sample program : sample_tf_broadcast.cpp
+ * @section main_sec6 The others.
+ * - \ref ROSCameraController
+ *  - sample program : sample_camera_controll.cpp
+ * - \ref ROSLEDController
+ *  - sample program : sample_led.cpp
  */
 #ifndef ROS_INTERFACE_HPP
 #define ROS_INTERFACE_HPP
